014d9fb73fb060c45777fe0fba1d6e1a6b412634
Zachary Folwick <zachary.folwick@rev.com>
2022-06-01 10:58:41 -0700

    matured the script somewhat

--- vagrantfiles/scripts/bootstrap.sh
+++ vagrantfiles/scripts/bootstrap.sh
@@ -5,19 +5,37 @@ YLW="\e[1;33m"     # yellow coloration
 NF="\e[0m"         # no-format of text color
 NL="\n"            # new line
 ENDL=${NF}${NL}    #end of formatted printf statements
-user=${whoami}
+user=$1
 
 echo hello, ${user}!
+wout () {
+  printf "%b" "${YLW}$1${ENDL}"
+}
+
+is_installed() {
+    if command -v $1 >/dev/null; then
+        true
+    else
+        false
+    fi
+    return
+}
+
+
 ############################################
 ## java install
 ############################################
-java_version="17-amazon-corretto-jdk"
-printf "%b" "${YLW}installing java version: ${java_version}"
-wget -O- https://apt.corretto.aws/corretto.key | apt-key add -
-add-apt-repository -y 'deb https://apt.corretto.aws stable main'
-apt-get install -y java-${java_version}
-echo $(java -version)
-printf "%b" "${YLW}installed java version: ${java_version}"
+
+if is_installed java ; then
+    wout "java is already installed. version is: $(java --version)"
+else
+    java_version="17-amazon-corretto-jdk"
+    wout "installing java version: ${java_version}"
+    wget -O- https://apt.corretto.aws/corretto.key | apt-key add -
+    add-apt-repository -y 'deb https://apt.corretto.aws stable main'
+    apt-get install -y java-${java_version}
+    wout "installed java version: ${java_version}"
+fi
 #
 ###########################################
 # /java install
@@ -26,17 +44,18 @@ printf "%b" "${YLW}installed java version: ${java_version}"
 ###########################################
 # maven install
 ###########################################
-maven_version="3.8.5"
-printf "%b" "${YLW}installing maven version: ${maven_version}${ENDL}"
+if is_installed mvn; then
+    wout "mvn already install. version is: $(mvn --version)"
+else
+    maven_version="3.8.5"
+    wout "installing maven version: ${maven_version}"
+    wget -q https://mirrors.estointernet.in/apache/maven/maven-3/${maven_version}/binaries/apache-maven-${maven_version}-bin.tar.gz
+    tar -xf apache-maven-${maven_version}-bin.tar.gz -C /opt
+    M2_HOME="/opt/apache-maven-${maven_version}"
+    ln -s $M2_HOME/bin/mvn /usr/bin/mvn
 
-wget -q https://mirrors.estointernet.in/apache/maven/maven-3/${maven_version}/binaries/apache-maven-${maven_version}-bin.tar.gz
-tar -xf apache-maven-${maven_version}-bin.tar.gz -C /opt
-M2_HOME="/opt/apache-maven-${maven_version}"
-export PATH="$M2_HOME/bin:$PATH"
-
-echo $(mvn -version)
-printf "%b" "${YLW}installed maven version: ${maven_version}${ENDL}"
-
+    wout "installed maven version: ${maven_version}"
+fi
 ###########################################
 # /maven install
 ###########################################
@@ -45,17 +64,24 @@ printf "%b" "${YLW}installed maven version: ${maven_version}${ENDL}"
 ###########################################
 # docker install
 ###########################################
-apt-get install -y apt-transport-https ca-certificates curl software-properties-common
-curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
-add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu focal stable"
-apt-cache policy docker-ce
-apt-get install -y docker-ce
-systemctl status docker
-printf "%b" "${YLW}installed docker${ENDL}"
-printf "%b" "${YLW}configuring docker${ENDL}"
-groupadd docker
-usermod -aG docker ${user}
-newgrp docker
+if is_installed docker; then
+    wout "docker is already installed"
+else
+    apt-get install -y apt-transport-https ca-certificates curl software-properties-common
+    curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
+    add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu focal stable"
+    apt-cache policy docker-ce
+    apt-get install -y docker-ce
+    systemctl status docker
+    wout "installed docker"
+    wout "configuring docker"
+    groupadd -f docker
+    usermod -aG docker ${user}
+    chown root:docker /var/run/docker.sock
+    #chown "${user}":"${user}" /home/"${user}"/.docker -R
+    #chmod g+rwx "/home/${user}/.docker" -R
+    #newgrp docker
+
 ###########################################
 # /docker install
 ###########################################
@@ -65,12 +91,16 @@ newgrp docker
 ###########################################
 # selenoid install
 ###########################################
-selenoid_version="1.8.1"
-chrome_version="99.0"
-printf "%b" "${YLW}installing selenoid UI version: ${selenoid_version}${ENDL}"
-wget -q https://github.com/aerokube/cm/releases/download/${selenoid_version}/cm_linux_amd64
-chmod +x cm_linux_amd64
-./cm_linux_amd64 selenoid start --browsers "chrome:${chrome_version}"
+if [ -x cm_linux_amd64 ] ; then
+    wout "selenoid alread installed"
+else
+    selenoid_version="1.8.1"
+    chrome_version="99.0"
+    wout "installing selenoid UI version: ${selenoid_version}"
+    wget -q https://github.com/aerokube/cm/releases/download/${selenoid_version}/cm_linux_amd64
+    chmod +x cm_linux_amd64
+    ./cm_linux_amd64 selenoid start --browsers "chrome:${chrome_version}"
+fi
 
 #
 echo completed updates and installs
